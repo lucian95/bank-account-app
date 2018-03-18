5dc4ea1bc886cf74aef95de61f8871714beb6aee
lucian95 <luciancondrea95@gmail.com>
2018-03-18 14:03:21 -0400

    Remove body-parser dependency (can use Express instead)

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('resources'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/resources/index.html'));
 
