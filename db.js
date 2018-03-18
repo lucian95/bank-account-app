849b7474a4cded741807aff1990de7c70ff7f02e
lucian95 <luciancondrea95@gmail.com>
2018-03-18 16:25:00 -0400

    Add engines and fix database url

--- db.js
+++ db.js
@@ -1,13 +1,12 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
-const url = process.env.MONGODB_URI || 'mongodb://localhost:27017';
-const dbName = process.env.MONGODB_DBNAME || 'BankDB';
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/BankDB';
 
 function getAccount(accountNumber, callback) {
 
   MongoClient.connect(url, function(err, client) {
     assert.equal(null, err);
-    const col = client.db(dbName).collection('Accounts');
+    const col = client.db('heroku_1qkkp8rg').collection('Accounts');
 
     col.findOne({accountNumber: accountNumber}, function(err, res) {
       assert.equal(null, err);
