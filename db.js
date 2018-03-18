b12b4236b2ba2b811792200cd984dd53d90e69ec
lucian95 <luciancondrea95@gmail.com>
2018-03-18 16:00:11 -0400

    Get dbName from env variable

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 const url = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+const dbName = process.env.MONGODB_DBNAME || 'BankDB';
 
 function getAccount(accountNumber, callback) {
 
   MongoClient.connect(url, function(err, client) {
     assert.equal(null, err);
-    const col = client.db('BankDB').collection('Accounts');
+    const col = client.db(dbName).collection('Accounts');
 
     col.findOne({accountNumber: accountNumber}, function(err, res) {
       assert.equal(null, err);
