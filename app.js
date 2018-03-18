b1ca17fc47723b2cabf027cec082e06f0ee84d6a
lucian95 <luciancondrea95@gmail.com>
2018-03-18 13:52:23 -0400

    Receive user request data correctly

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('resources'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/resources/index.html'));
+
+app.get('/view-account', (req, res) => {
+  console.log('User wants to view account', req.query.accountNumber);
+  res.send('');
+});
+
+app.post('/request-account', (req, res) => {
+  console.log('User wants to request a new account with this info ', req.body);
+  res.send('');
+});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
