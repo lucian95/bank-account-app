baadd048d9bc46d81802c864678544d87ee5aa3d
lucian95 <luciancondrea95@gmail.com>
2018-03-17 21:40:19 -0400

    Improve readability and security of script

--- resources/script.js
+++ resources/script.js
@@ -1,18 +1,21 @@
-document.getElementById('view-account-button').onclick = function() {
-  document.getElementById('main-menu-div').hidden = true;
-  document.getElementById('view-account-div').hidden = false;
-};
-
-document.getElementById('request-account-button').onclick = function() {
-  document.getElementById('main-menu-div').hidden = true;
-  document.getElementById('request-account-div').hidden = false;
-};
-
-for (let button of document.getElementsByClassName('return-to-menu-button')) {
-  button.onclick = function() {
-    // It does not matter specifically which div is clicked
-    document.getElementById('request-account-div').hidden = true;
-    document.getElementById('view-account-div').hidden = true;
-    document.getElementById('main-menu-div').hidden = false;
+(function() {
+  document.getElementById('view-account-button').onclick = function() {
+    document.getElementById('main-menu-div').hidden = true;
+    document.getElementById('view-account-div').hidden = false;
   };
-}
+
+  document.getElementById('request-account-button').onclick = function() {
+    document.getElementById('main-menu-div').hidden = true;
+    document.getElementById('request-account-div').hidden = false;
+  };
+
+  let buttons = document.getElementsByClassName('return-to-menu-button');
+  for (let button of buttons) {
+    button.onclick = function() {
+      // It does not matter specifically which div is clicked
+      document.getElementById('request-account-div').hidden = true;
+      document.getElementById('view-account-div').hidden = true;
+      document.getElementById('main-menu-div').hidden = false;
+    };
+  }
+})()
