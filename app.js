f9946cfdcaef0bbbe5d79fef833e948a80f77e5f
lucian95 <luciancondrea95@gmail.com>
2018-03-17 20:29:38 -0400

    Hook buttons to their events

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static('resources'));
+
 app.get('/', (req, res) => res.sendFile(__dirname + '/resources/index.html'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ resources/script.js
@@ -0,0 +1,7 @@
+document.getElementById('view-account-button').onclick = function() {
+  console.log('User wants to view an account');
+};
+
+document.getElementById('request-account-button').onclick = function() {
+  console.log('User wants to request an account');
+};
